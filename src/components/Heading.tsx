76140c654fafb6b6c6119c47818a3b578515aa11
Jaine Scruph <jscruph10@gmail.com>
2022-10-19 11:37:01 -0300

    Creating project homepage

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -4,20 +4,25 @@ import { Slot } from '@radix-ui/react-slot';
 import clsx from 'clsx';
 
 export interface HeadingProps {
-  size?: 'sm' | 'md' | 'lg';
-  children: ReactNode;
   asChild?: boolean;
+  className?: string;
+  children: ReactNode;
+  size?: 'sm' | 'md' | 'lg';
 }
 
-export function Heading({ size = 'md', children, asChild }: HeadingProps){
+export function Heading({ size = 'md', children, asChild, className }: HeadingProps){
   const Comp = asChild ? Slot : 'h2';
 
   return (
-    <Comp className={clsx('text-gray-100 font-bold font-sans', {
-      'text-lg': size === 'sm',
-      'text-xl': size === 'md',
-      'text-2xl': size === 'lg',
-    })}>
+    <Comp className={clsx(
+      'text-gray-100 font-bold font-sans', 
+      {
+        'text-lg': size === 'sm',
+        'text-xl': size === 'md',
+        'text-2xl': size === 'lg',
+      },
+      className
+    )}>
       {children}
     </Comp>
   )
