4248d35d51f16c6ffa267753fb0784205d1cc7af
Jaine Scruph <jscruph10@gmail.com>
2022-10-20 10:55:36 -0300

    After

---
+++ src/pages/SignIn.stories.tsx
@@ -0,0 +1,42 @@
+import { expect } from '@storybook/jest'
+import { Meta, StoryObj } from '@storybook/react'
+import { within, userEvent, waitFor } from '@storybook/testing-library'
+
+import { SignIn } from "./SignIn";
+
+import { rest } from 'msw';
+
+export default {
+  title: 'pages/Sign in',
+  component: SignIn,
+  args: {},
+  argTypes: {},
+  parameters: {
+    msw: {
+      handlers: [
+        rest.post('/sessions', (req, res, ctx) => {
+          return res(ctx.json({
+            message: 'Login realizado!'
+          }))
+        })
+      ]
+    }
+  }
+} as Meta
+
+export const Default: StoryObj = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    userEvent.type(canvas.getByPlaceholderText('Digite seu e-mail'), 'jscruph10@gmail.com')
+    userEvent.type(canvas.getByPlaceholderText('***********'), '12345678')
+
+    userEvent.click(canvas.getByRole('button'))
+
+    //cria um set interval, roda o código várias vezes até passar
+    await waitFor(() => {
+      return expect(canvas.getByText('Login realizado!')).toBeInTheDocument()
+    })
+
+  }
+}
